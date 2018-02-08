71f427e753786b3f99f0d55ecf864d7185b6fe9c
a15566 <a15566@d1-05-02.dped.local>
2018-02-08 15:37:29 +0000

    Versão 1.0

---
+++ src/m17b_fichaaval/Main.java
@@ -0,0 +1,14 @@
+package m17b_fichaaval;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ponto2D p1 = new Ponto2D(5,7);
+        Ponto2D p2 = new Ponto2D(2,8);
+        
+        System.out.println(p1);
+        System.out.println(p2);
+        
+        System.out.println(p2.distancia(p1));
+    }
+}
